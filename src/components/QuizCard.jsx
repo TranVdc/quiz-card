0778ef8c2eb867c61b68ba656362f542c75e02b2
Tran Vdc <tranvanduoc2394@gmail.com>
2024-05-09 14:33:44 +0700

    fix css in mobile

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -6,14 +6,12 @@ export default function QuizCard({ quiz }) {
     <div key={quiz.id} className={styles.quizCardContainer}>
       <div className={styles.quizCardInner}>
         <div className={styles.quizCardFront}>
-          <div>
-            <h3 className={styles.questionContent}>{quiz.question}</h3>
-            <ol className={styles.optionList}>
-              {quiz.options.map((option) => (
-                <li>{option}</li>
-              ))}
-            </ol>
-          </div>
+          <h3 className={styles.questionContent}>{quiz.question}</h3>
+          <ol className={styles.optionList}>
+            {quiz.options.map((option) => (
+              <li>{option}</li>
+            ))}
+          </ol>
         </div>
         <div className={styles.quizCardBack}>{quiz.answer}</div>
       </div>
